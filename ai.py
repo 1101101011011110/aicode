eb9e63b423b04809b823963caea42518c2fb701e
dplavan <plavan@balancesystems.it>
2023-04-29 19:48:26 +0200

    API key from Environment

--- ai.py
+++ ai.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key  = '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'
+openai.api_key  = os.environ['OPENAI_API_KEY']
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
